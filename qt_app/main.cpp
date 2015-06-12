af65b6989acf7610d3a58762be22e6133e764a58
vasily-knk <vasily.knk@gmail.com>
2015-06-12 22:07:30 +0300

    qimg

--- qt_app/main.cpp
+++ qt_app/main.cpp
@@ -1,9 +1,33 @@
+struct my_widget
+    : QWidget
+{
+    my_widget(QImage *img)
+        : QWidget(nullptr)
+        , img_(img)
+    {}
+
+    void paintEvent(QPaintEvent *) 
+    {
+        QPainter p(this);
+        p.drawImage(QPoint(0, 0), img_);
+    }
+
+private:
+    QImage *img_;
+};
+
 int main(int argv, char **args)
 {
     QApplication app(argv, args);
 
-    QTextEdit textEdit;
-    textEdit.show();
+    vector<char> buffer(256 * 256 * 4, 0);
+
+    my_widget w;
+
+    QVBoxLayout *layout = new QVBoxLayout(&w);
+    QImage *img = new QImage(reinterpret_cast<uchar*>(buffer.data()), 256, 256, QImage::Format_RGBA8888);
+
+    w.show();
 
     return app.exec();
 }
--- qt_app/stdafx.h
+++ qt_app/stdafx.h
@@ -2,3 +2,8 @@
 
 #include <QApplication>
 #include <QTextEdit>
+#include <QImage>
+#include <QVBoxLayout>
+
+#include <vector>
+using std::vector;
